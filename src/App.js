c720ab275a2bcd3aaaeff8a0e41006c6fca36fef
niharikanagaraj <niharikagnnayaka@gmail.com>
2026-04-28 22:15:19 +0530

    Ceated Footer Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
+import HomeScreen from "./Screens/HomeScreen";
+
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
     <Header/>
     <Container>
       <main>
+        <HomeScreen/>
 
       </main>
 
---
+++ src/Screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container,Row,Col}from 'react-bootstrap';
+
+const HomeScreen=()=>{
+    return(
+        <div>
+            <marquee>VTU Results 2026 out check Exam Results here</marquee>
+        </div>
+    
+    )
+}
+export default HomeScreen
